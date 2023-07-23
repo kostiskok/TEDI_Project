5f55104afdb9a01efe381bb16406f30d86010cee
Kostis Kokolias <kostisk22@gmail.com>
2023-07-23 23:54:09 +0300

    Comment some stuff from tutorials

--- backend/airbnb/models.py
+++ backend/airbnb/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from submodels/Room import Room
+
 # Create your models here.
 
-class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+# class Todo(models.Model):
+#     title = models.CharField(max_length=120)
+#     description = models.TextField()
+#     completed = models.BooleanField(default=False)
 
-    def _str_(self):
-        return self.titl
+#     def _str_(self):
+#         return self.titl
