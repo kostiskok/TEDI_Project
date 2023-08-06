e600c8dc3975721adb8da2db4274247185f55ff8
Kostis Kokolias <kostisk22@gmail.com>
2023-08-06 13:38:35 +0300

    views/serializers testing
    Testing api views, decorators and class based api views

--- backend/airbnb/urls.py
+++ backend/airbnb/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 # from .views import Index
+# from .views import test_list, test_details
+from .views import TestList, TestDetails
 
 urlpatterns = [
     # path('', Index)
+    # path('tests/', test_list),
+    # path('tests/<int:pk>/', test_details),
+    path('tests/', TestList.as_view()),
+    path('tests/<int:id>/', TestDetails.as_view())
 ]
 
--- backend/airbnb/views.py
+++ backend/airbnb/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+
+# from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import APIView
+
 
 from .models import Test
 from .serializers import TestSerializer
@@ -15,10 +24,95 @@ from .serializers import TestSerializer
 
 # def Index(requst):
 #     return HttpResponse("It is working")
-
+'''
+# @csrf_exempt
+@api_view(['GET', 'POST'])
 def test_list(request):
     
     #get all tests
     if request.method == 'GET':
         tests = Test.objects.all()
         serializer = TestSerializer(tests, many=True)
+        # return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        # data = JSONParser().parse(request)
+        serializer = TestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# @csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def test_details(request, pk):
+    try:
+        test = Test.objects.get(pk=pk)
+        
+    except Test.DoesNotExist:
+        # return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = TestSerializer(test)
+        # return JsonResponse(serializer.data)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        # data = JSONParser().parse(request)
+        # serializer = TestSerializer(test, data=data)
+        serializer = TestSerializer(test, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # return JsonResponse(serializer.data)
+            return Response(serializer.data)
+        # return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        test.delete()
+        # return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+class TestList(APIView):
+    
+    def get(self, request):
+        tests = Test.objects.all()
+        serializer = TestSerializer(tests, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class TestDetails(APIView):
+    
+    def get_object(self, id):
+        try:
+            return Test.objects.get(id=id)
+        except Test.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def get(self, request, id):
+        test = self.get_object(id)
+        serializer = TestSerializer(test)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        test = self.get_object(id)
+        serializer = TestSerializer(test, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id):
+        test = self.get_object(id)
+        test.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
